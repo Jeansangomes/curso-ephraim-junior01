3dc52ab6b990c9365c9df7669aa740a3d8697571
Jean Gomes <88942286+Jeansangomes@users.noreply.github.com>
2022-07-21 15:33:59 -0300

    criando js

---
+++ script.js
@@ -0,0 +1,58 @@
+function initAccordion() {
+  const accordionList = document.querySelectorAll('.js-accordion dt');
+  const activeClass = 'ativo';
+  if (accordionList.length) {
+    accordionList[0].classList.add(activeClass);
+    accordionList[0].nextElementSibling.classList.add(activeClass);
+
+    function activeAcordion(event) {
+      this.classList.toggle(activeClass);
+      this.nextElementSibling.classList.toggle(activeClass);
+    }
+    accordionList.forEach((item) => {
+      item.addEventListener('click', activeAcordion);
+    });
+  }
+}
+initAccordion();
+
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    const topo = section.offsetTop;
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  }
+
+  // form Alternativa de scroll suave
+  /*
+  window.scrollTo({
+    top: topo,
+    behavior: 'smooth'
+  })*/
+
+  linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+  });
+}
+initScrollSuave();
+
+const sections = document.querySelectorAll('.js-scroll');
+
+function animaScroll() {
+  sections.forEach((section) => {
+    const sectionTop = section.getBoundingClientRect().top;
+    console.log(sectionTop);
+    if (sectionTop < 0) {
+      console.log('animar');
+    }
+  });
+}
+
+window.addEventListener('scroll', animaScroll);
