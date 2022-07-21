0de7cc65612f4a2983619400a34e77476820614f
Jean Gomes <88942286+Jeansangomes@users.noreply.github.com>
2022-07-21 16:01:13 -0300

    scroll

--- script.js
+++ script.js
@@ -43,18 +43,29 @@ function initScrollSuave() {
 }
 initScrollSuave();
 
-const sections = document.querySelectorAll('.js-scroll');
-const windowMetade = window.innerHeight * 0.6;
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll('.js-scroll');
 
-function animaScroll() {
-  sections.forEach((section) => {
-    const sectionTop = section.getBoundingClientRect().top - windowMetade;
-    const isSectionVisible = sectionTop - windowMetade < 0;
+  if (sections.length) {
+    const windowMetade = window.innerHeight * 0.6;
 
-    if (isSectionVisible) {
-      section.classList.add('ativo');
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top - windowMetade;
+        const isSectionVisible = sectionTop - windowMetade < 0;
+
+        if (isSectionVisible) {
+          section.classList.add('ativo');
+        } else {
+          section.classList.remove('ativo');
+        }
+      });
     }
-  });
+
+    animaScroll();
+
+    window.addEventListener('scroll', animaScroll);
+  }
 }
 
-window.addEventListener('scroll', animaScroll);
+initAnimacaoScroll();
