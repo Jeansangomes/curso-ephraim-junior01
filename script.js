296e28a1471f0cb4fc2d926510b9e159070fb704
Jean Gomes <88942286+Jeansangomes@users.noreply.github.com>
2022-07-21 15:49:15 -0300

    kk

--- script.js
+++ script.js
@@ -44,12 +44,14 @@ function initScrollSuave() {
 initScrollSuave();
 
 const sections = document.querySelectorAll('.js-scroll');
+const windowMetade = window.innerHeight * 0.6;
 
 function animaScroll() {
   sections.forEach((section) => {
-    const sectionTop = section.getBoundingClientRect().top;
-    console.log(sectionTop);
-    if (sectionTop < 0) {
+    const sectionTop = section.getBoundingClientRect().top - windowMetade;
+    const isSectionVisible = sectionTop - windowMetade < 0;
+
+    if (isSectionVisible) {
       section.classList.add('ativo');
     }
   });
